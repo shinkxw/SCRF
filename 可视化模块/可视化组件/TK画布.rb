e1fd802ec9dde20230334e77799ac0e2247b4df0
shink <shink_7770@qq.com>
2015-05-21 01:21:02 +0800

    椭圆能够改变位置

--- 可视化模块/可视化组件/TK画布.rb
+++ 可视化模块/可视化组件/TK画布.rb
@@ -11,39 +11,48 @@ class TK画布 < K可视化组件
   def 点转换(点);(@y轴逆转 ? 点.类.新建(点.x, @高 - 点.y) : 点) end
 end
 
-class TKC线段 < K可视化组件
+class K画布组件 < K可视化组件
+  def 点转换(点);@父组件.点转换(点) end
+end
+
+class TKC线段 < K画布组件
   attr_reader :起点, :终点
   def initialize(画布, 起点, 终点);super(画布); @起点, @终点 = 起点, 终点 end
   def 实例指令
-    起点, 终点 = @父组件.点转换(@起点), @父组件.点转换(@终点)
+    起点, 终点 = 点转换(@起点), 点转换(@终点)
     "TkcLine.new(父实例, #{起点.x}, #{起点.y}, #{终点.x}, #{终点.y})"
   end
   def 箭头(参数 = 'last');返回自己{实例.arrow(参数)} end#first,last,both
 end
 
-class TKC矩形 < K可视化组件
+class TKC矩形 < K画布组件
   attr_reader :起点, :终点
   def initialize(画布, 起点, 终点);super(画布); @起点, @终点 = 起点, 终点 end
   def 实例指令
-    起点, 终点 = @父组件.点转换(@起点), @父组件.点转换(@终点)
+    起点, 终点 = 点转换(@起点), 点转换(@终点)
     "TkcRectangle.new(父实例, #{起点.x}, #{起点.y}, #{终点.x}, #{终点.y})"
   end
 end
 
-class TKC椭圆 < K可视化组件
-  attr_reader :左点, :右点
+class TKC椭圆 < K画布组件
+  attr_reader :左点, :右点#边界矩形左上角和底部右下角
   def initialize(画布, 左点, 右点);super(画布); @左点, @右点 = 左点, 右点 end
   def 实例指令
-    左点, 右点 = @父组件.点转换(@左点), @父组件.点转换(@右点)
+    左点, 右点 = 点转换(@左点), 点转换(@右点)
     "TkcOval.new(父实例, #{左点.x}, #{左点.y}, #{右点.x}, #{右点.y})"
+  end
+  def 改变位置(左点, 右点)
+    @左点, @右点 = 左点, 右点
+    左点, 右点 = 点转换(@左点), 点转换(@右点)
+    父实例.coords(实例, 左点.x, 左点.y, 右点.x, 右点.y)
   end
 end
 
-class TKC文本 < K可视化组件
+class TKC文本 < K画布组件
   attr_reader :起点
   def initialize(画布, 起点);super(画布); @起点 = 起点 end
   def 实例指令
-    起点 = @父组件.点转换(@起点)
+    起点 = 点转换(@起点)
     "TkcText.new(父实例, #{起点.x}, #{起点.y})"
   end
   def 文本=(文本);实例.text(文本) end
