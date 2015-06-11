535ac789521e3964cffa56d9050634a9dea2869f
shink <shink_7770@qq.com>
2015-06-12 00:28:06 +0800

    性能分析器部分中文化

--- 性能分析模块/S性能分析器.rb
+++ 性能分析模块/S性能分析器.rb
@@ -1,68 +1,55 @@
 ﻿#!/usr/bin/env ruby -w
 # encoding: UTF-8
 module S性能分析器
-  class Wrapper < Struct.new(:defined_class, :method_id, :hash) # :nodoc:
-    private :defined_class=, :method_id=, :hash=
-
-    def initialize(klass, mid)
-      super(klass, mid, nil)
-      self.hash = klass.hash + mid.hash
+  class Wrapper < Struct.new(:类, :方法名, :hash)
+    private :类=, :方法名=, :hash=
+    def initialize(类, 方法名)
+      super(类, 方法名, nil)
+      self.hash = 类.hash + 方法名.hash
     end
-
-    def to_s
-      ("#{defined_class.inspect}#".sub(/\A\#<Class:(.*)>#\z/, '\1.') << method_id.to_s).中文化
-    end
+    def to_s;("#{类.inspect}#".sub(/\A\#<Class:(.*)>#\z/, '\1.') << 方法名.to_s).中文化 end
     alias inspect to_s
   end
 
   # internal values
-  @@start = nil # the start time that profiling began
+  @@开始时间 = nil
   @@stacks = nil # the map of stacks keyed by thread
   @@maps = nil # the map of call data keyed by thread, class and id. Call data contains the call count, total time,
-  PROFILE_CALL_PROC = TracePoint.new(*%i[call c_call b_call]) {|tp| # :nodoc:
-    now = Process.times[0]
+  PROFILE_CALL_PROC = TracePoint.new(*%i[call c_call b_call]) {|tp|
+    当前时间 = Process.times[0]
     stack = (@@stacks[Thread.current] ||= [])
-    stack.push [now, 0.0]
+    stack.push [当前时间, 0.0]
   }
-  PROFILE_RETURN_PROC = TracePoint.new(*%i[return c_return b_return]) {|tp| # :nodoc:
-    now = Process.times[0]
+  PROFILE_RETURN_PROC = TracePoint.new(*%i[return c_return b_return]) {|tp|
+    当前时间 = Process.times[0]
     key = Wrapper.new(tp.defined_class, tp.method_id)
     stack = (@@stacks[Thread.current] ||= [])
     if tick = stack.pop
       threadmap = (@@maps[Thread.current] ||= {})
       data = (threadmap[key] ||= [0, 0.0, 0.0, key])
       data[0] += 1
-      cost = now - tick[0]
+      cost = 当前时间 - tick[0]
       data[1] += cost
       data[2] += cost - tick[1]
       stack[-1][1] += cost if stack[-1]
     end
   }
 module_function
-  # Starts the profiler.
-  #
-  # See Profiler__ for more information.
-  def start_profile
-    @@start = Process.times[0]
+  def 开始分析
+    @@开始时间 = Process.times[0]
     @@stacks = {}
     @@maps = {}
     PROFILE_CALL_PROC.enable
     PROFILE_RETURN_PROC.enable
   end
-  # Stops the profiler.
-  #
-  # See Profiler__ for more information.
-  def stop_profile
+  def 停止分析
     PROFILE_CALL_PROC.disable
     PROFILE_RETURN_PROC.disable
   end
-  # Outputs the results from the profiler.
-  #
-  # See Profiler__ for more information.
-  def print_profile(f)
-    stop_profile
-    total = Process.times[0] - @@start
-    if total == 0 then total = 0.01 end
+  def 输出分析结果(f = STDERR)
+    停止分析
+    总耗时 = Process.times[0] - @@开始时间
+    总耗时 = 0.01 if 总耗时 == 0
     totals = {}
     @@maps.values.each do |threadmap|
       threadmap.each do |key, data|
@@ -73,27 +60,25 @@ module_function
       end
     end
 
-    # Maybe we should show a per thread output and a totals view?
-
     data = totals.values
     data = data.sort_by{|x| -x[2]}
     sum = 0
-    f.printf "  %%   cumulative   self              self     total\n"
+    f.printf "   %%   cumulative   self                   self      total\n"
     f.printf " time   seconds   seconds    calls  ms/call  ms/call  name\n"
     for d in data
       sum += d[2]
-      f.printf "%6.2f %8.2f  %8.2f %8d ", d[2]/total*100, sum, d[2], d[0]
+      f.printf "%6.2f %8.2f  %8.2f %8d ", d[2]/总耗时*100, sum, d[2], d[0]
       f.printf "%8.2f %8.2f  %s\n", d[2]*1000/d[0], d[1]*1000/d[0], d[3]
     end
-    f.printf "%6.2f %8.2f  %8.2f %8d ", 0.0, total, 0.0, 1     # ???
-    f.printf "%8.2f %8.2f  %s\n", 0.0, total*1000, "#toplevel" # ???
+    f.printf "%6.2f %8.2f  %8.2f %8d ", 0.0, 总耗时, 0.0, 1     # ???
+    f.printf "%8.2f %8.2f  %s\n", 0.0, 总耗时*1000, "#toplevel" # ???
   end
 end
 RubyVM::InstructionSequence.compile_option = {
-  #:trace_instruction => true,
-  #:specialized_instruction => false
+  :trace_instruction => true,
+  :specialized_instruction => false
 }
 END {
-  S性能分析器::print_profile(STDERR)
+  S性能分析器::输出分析结果
 }
-S性能分析器::start_profile
+S性能分析器::开始分析
--- 系统打开类/String.rb
+++ 系统打开类/String.rb
@@ -3,6 +3,8 @@
 class String
   def 编码变动(原编码,现编码);self.force_encoding(原编码).encode(现编码) end
   def 中文化;编码为('GBK') end
+  def 中文长度;length + encode('utf-8').scan(/[\u4E00-\u9FA5]/).size end
+  def 填充(长度, 填充符 = ' ');中文长度 < 长度 ? self + (填充符 * (长度 - 中文长度)) : self end
   alias :长度 :length
   alias :编码 :encoding
   alias :符号 :to_sym
