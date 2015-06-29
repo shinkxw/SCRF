4712375aeb9040d07fcfbf115e253691a544e88c
shink <shink_7770@qq.com>
2015-06-29 22:02:21 +0800

    可视化模块增加延迟显示功能

--- 可视化模块/K可视化模块.rb
+++ 可视化模块/K可视化模块.rb
@@ -8,4 +8,5 @@ module K可视化模块
   def self.全部实例化;待实例组件.每个(&:实例) end
   def self.运行;全部实例化;Tk.mainloop end
   def self.后台运行;全部实例化;R线程.new {Tk.mainloop} end
+  def self.延迟显示(最大延迟毫秒数 = 300,&运行块);TkAfter.start(最大延迟毫秒数, 1, &运行块) end
 end
