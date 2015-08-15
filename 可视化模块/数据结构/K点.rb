d39fc6c979174f3e5cf2512319d46c780c5884ba
shink <shink_7770@qq.com>
2015-08-15 14:14:29 +0800

    删除可视化模块

--- 可视化模块/数据结构/K点.rb
+++
@@ -1,9 +0,0 @@
-﻿#!/usr/bin/env ruby -w
-# encoding: UTF-8
-class K点 < D二维点
-  def 旋转弧度(弧度 = Math::PI / 4)
-    x = (Math.cos(弧度)* @x - Math.sin(弧度) * @y).四舍五入
-    y = (Math.cos(弧度)* @y + Math.sin(弧度) * @x).四舍五入
-    K点.新建(x, y)
-  end
-end
