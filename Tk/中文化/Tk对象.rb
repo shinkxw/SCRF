5bf58c9aef768cff067cd69fef606860f75d7aec
shink <shink_7770@qq.com>
2015-08-16 21:33:43 +0800

    method_missing中文化

--- Tk/中文化/Tk对象.rb
+++ Tk/中文化/Tk对象.rb
@@ -7,9 +7,9 @@ class TkObject
       定义方法(现方法名 + '=') {|值| 调用(原方法名, 值)}
     end
   end
-  alias :tk_method_missing :method_missing
-  def method_missing(方法名, *参数)
+  alias :tk方法缺失 :method_missing
+  def 方法缺失(方法名, *参数)
     return 调用($1, *参数) if 方法名.字符串化 =~ /^([\u4e00-\u9fa5]+)=$/
-    tk_method_missing(方法名, *参数)
+    tk方法缺失(方法名, *参数)
   end
 end
--- 系统打开类/Module.rb
+++ 系统打开类/Module.rb
@@ -11,8 +11,10 @@ class Module
   alias :字符串 :to_s
   alias :重命名方法 :alias_method
   alias :_old_method_added_ :method_added
+  @@中文化方法映射 = {初始化: :initialize, 方法缺失: :method_missing}
   def method_added(方法名称)
-    重命名方法(:initialize, :初始化) if 方法名称 == :初始化
+    原方法名 = @@中文化方法映射[方法名称]
+    重命名方法(原方法名, 方法名称) if 原方法名
     _old_method_added_(方法名称)
   end
 end
