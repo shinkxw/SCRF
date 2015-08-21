80761944fe7d4cb058e4787950b12d34c6ea686e
shinkxw <shink_7770@qq.com>
2015-08-21 16:59:27 +0800

    中文化

--- 系统打开类/Range.rb
+++ 系统打开类/Range.rb
@@ -3,8 +3,13 @@
 class Range
   def 随机; 起点 + Kernel.rand(终点 - 起点 + 1) end
   def 差值; 终点 - 起点 end
-  alias :起点 :first
-  alias :终点 :last
+  alias :起点 :begin
+  alias :终点 :end
+  alias :前 :first
+  alias :后 :last
+  alias :大小 :size
+  alias :覆盖? :cover?#("a".."z").cover?("cc")  #=> true
+  alias :包含? :include?#("a".."z").include?("cc")  #=> false
   alias :数组化 :to_a
   alias :字符串 :to_s
   alias :每个 :each
