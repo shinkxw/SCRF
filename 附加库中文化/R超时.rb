ad867a26bea0ceef6e1ced626c4e1d60dc5ac211
shink <shink_7770@qq.com>
2015-08-22 20:48:19 +0800

    增加超时库的中文化及调用

---
+++ 附加库中文化/R超时.rb
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+需要 'timeout'
+R超时 = Timeout
+class << R超时
+  alias :超时 :timeout
+end
+
+module Kernel
+  def 限时(限制秒数, 超时异常 = E超时异常, &块);R超时::超时(限制秒数, 超时异常, &块) end
+end
+
+class E超时异常 < E异常
+  def 初始化(输入);super('超时异常', '运行块超过指定时间') end
+end
