f93b7e2d8b6731ccb07bf72ea964abc75c6e0ce7
shink <shink_7770@qq.com>
2015-08-24 21:01:47 +0800

    使用**0.5代替Math.sqrt,提高为3倍速度

--- 系统打开类/Numeric.rb
+++ 系统打开类/Numeric.rb
@@ -5,7 +5,7 @@ class Numeric
   def 较大(other); self > other ? self : other end
   def 最小二指数; 0.upto(50){|i| return 2**i if 2**i >= self} end
   def 平方; self**2 end
-  def 开根; Math.sqrt(self) end
+  def 开根; self**0.5 end#Math.sqrt(self)
   alias :绝对值 :abs
   alias :向上取整 :ceil
   alias :向下取整 :floor
