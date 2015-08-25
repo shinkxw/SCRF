364678b75ba2f44ae753bbcb312a4bcc1332b813
shink <shink_7770@qq.com>
2015-08-25 23:33:34 +0800

    中文化

--- 系统打开类/Struct.rb
+++ 系统打开类/Struct.rb
@@ -1,5 +1,20 @@
 ﻿#!/usr/bin/env ruby -w
 # encoding: UTF-8
-class << Struct
-  alias :新建 :new
+class Struct
+  class << self
+    alias :新建 :new
+    def inherited(继承类)
+      继承类.执行 do
+        单例类.执行 do
+          定义方法(:新建) do |*参数|
+            单例类.执行 do
+              重命名方法(:新建, :new)
+            end
+            新建(*参数)
+          end
+        end
+        重命名方法(:字符串, :to_s)
+      end
+    end
+  end
 end
