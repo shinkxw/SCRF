2d399956f2ea37606d4796b839e2f9b7ff400b78
shink <shink_7770@qq.com>
2015-08-30 12:47:24 +0800

    为方法缺失增加配对的幽灵方法?

--- Tk/中文化/Tk对象.rb
+++ Tk/中文化/Tk对象.rb
@@ -12,4 +12,7 @@ class TkObject
     return 调用($1, *参数) if 方法名.字符串化 =~ /^([\u4e00-\u9fa5]+)=$/
     tk方法缺失(方法名, *参数)
   end
+  def 幽灵方法?(方法名, 包括私有 = false)
+    方法名.字符串化 =~ /^([\u4e00-\u9fa5]+)=$/
+  end
 end
--- 系统打开类/Module.rb
+++ 系统打开类/Module.rb
@@ -11,7 +11,8 @@ class Module
   alias :字符串 :to_s
   alias :重命名方法 :alias_method
   alias :_old_method_added_ :method_added
-  @@中文化方法映射 = {初始化: 'initialize', 方法缺失: 'method_missing', 每个: 'each'}
+  @@中文化方法映射 = {初始化: 'initialize', 每个: 'each'}
+  @@中文化方法映射.merge!({方法缺失: 'method_missing', 幽灵方法?: 'respond_to_missing?'})
   def method_added(方法名称)
     原方法名 = @@中文化方法映射[方法名称]
     重命名方法(原方法名, 方法名称) if 原方法名
