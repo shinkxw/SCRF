d436f42fd5292822f16205563bfda3920f5b14b5
shink <shink_7770@qq.com>
2015-09-11 23:01:57 +0800

    中文化

--- 核心/中文化/Float.rb
+++ 核心/中文化/Float.rb
@@ -5,5 +5,6 @@ class Float
   alias :向上取整 :ceil
   alias :向下取整 :floor
   alias :四舍五入 :round
+  alias :浮点化 :to_f
   alias :字符串 :to_s
 end
---
+++ 核心/内核扩展/Float.rb
@@ -0,0 +1,5 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class Float
+  def 去零字符串;格式化("%g", self) end
+end
