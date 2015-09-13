2a7138ccc38157c716d93354203a485b66c5e291
shink <shink_7770@qq.com>
2015-09-13 18:53:23 +0800

    折线图增加了每条折线的色块与说明

--- 附加模块/Tk/核心/Tk组件/Tkc文本.rb
+++ 附加模块/Tk/核心/Tk组件/Tkc文本.rb
@@ -1,8 +1,8 @@
 ﻿#!/usr/bin/env ruby -w
 # encoding: UTF-8
 class Tkc文本
-  def self.创建(画布, 起点)
-    新建(画布, 起点.x, 起点.y)
+  def self.创建(画布, 起点, 文本 = '')
+    新建(画布, 起点.x, 起点.y){文本(文本)}
   end
   定义映射方法(text: '文本', font: '字体', underline: '下划线', anchor: '锚定', justify: '对齐')
   定义映射方法(fill: '填充色', offset: '偏移量', stipple: '点画', width: '宽度')
