101725f71c4fdffe63e852eaae50f051d385f792
shink <shink_7770@qq.com>
2015-09-15 23:02:03 +0800

    Tk画布中文化

--- 附加模块/Tk/核心/Tk组件/Tk画布.rb
+++ 附加模块/Tk/核心/Tk组件/Tk画布.rb
@@ -12,11 +12,25 @@ class Tk画布
     x轴卷动增量(x轴增量)
     y轴卷动增量(y轴增量)
   end
+  alias :删除 :delete
   alias :移动组件 :move#tag,dx,dy
   alias :移动组件至 :move_to#tag,dx,dy
+  alias :改变组件位置 :coords#组件,参数
+  alias :提升 :raise
+  alias :降低 :lower
+  alias :查找 :find
+  alias :画布x :canvasx
+  alias :画布y :canvasy
   alias :x轴卷动 :xview_scroll
   alias :y轴卷动 :yview_scroll
-  alias :改变组件位置 :coords#组件,参数
-  定义映射方法(width: '宽度', height: '高度', bg: '背景色')
+  alias :包围盒 :bbox#所有项目 all
+  定义映射方法(width: '宽度', height: '高度', bg: '背景色', bd: '边框宽度')
+  定义映射方法(confine: '限制范围', scrollregion: '滚动区域')
   定义映射方法(xscrollincrement: 'x轴卷动增量', yscrollincrement: 'y轴卷动增量')
+  定义映射方法(state: '状态', offset: '偏移量', cursor: '光标')
 end
+#~ ["closeenough", "closeEnough", "CloseEnough", 1, 1.0]#鼠标接近多少算是被认为在项目内
+#~ ["relief", "relief", "Relief", "flat", "flat"]#3D样式
+#~ ["takefocus", "takeFocus", "TakeFocus", false, false]#是否被当做焦点
+#~ ["xscrollcommand", "xScrollCommand", "ScrollCommand", "", ""]#回调
+#~ ["yscrollcommand", "yScrollCommand", "ScrollCommand", "", ""]#回调
