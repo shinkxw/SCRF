7f78003250ad660fccce89789ec59b50b92c23dc
shink <shink_7770@qq.com>
2015-09-16 20:50:45 +0800

    为Tk的延迟执行封装了错误抓取与输出

---
+++ 核心/中文化/Proc.rb
@@ -0,0 +1,6 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class Proc
+  alias :执行 :call
+  alias :参数个数 :arity
+end
--- 附加模块/Tk/核心/Tk.rb
+++ 附加模块/Tk/核心/Tk.rb
@@ -5,5 +5,7 @@ module Tk
     alias :运行 :mainloop
   end
   def self.后台运行;R线程.新建 {运行} end
-  def self.延迟显示(最大延迟毫秒数 = 300,&运行块);TkAfter.start(最大延迟毫秒数, 1, &运行块) end
+  def self.延迟显示(最大延迟毫秒数 = 300,&运行块)
+    TkAfter.start(最大延迟毫秒数, 1){忽略错误{运行块.执行}}
+  end
 end
