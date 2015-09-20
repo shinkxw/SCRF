8d1b14fb7a87cae1e1fa592e0b2bd6e1a4cf923a
shink <shink_7770@qq.com>
2015-09-20 23:39:36 +0800

    Tk扩展

--- 附加模块/Tk/核心/Tk组件/Tkc椭圆.rb
+++ 附加模块/Tk/核心/Tk组件/Tkc椭圆.rb
@@ -4,9 +4,8 @@ class Tkc椭圆
   def self.创建(画布, 左上点, 右下点)
     新建(画布, 左上点.x, 左上点.y, 右下点.x, 右下点.y)
   end
-  def 改变位置(画布, 左点, 右点)
-    画布.改变组件位置(self, 左点.x, 左点.y, 右点.x, 右点.y)
-  end
+  def 移动(画布, 移动向量);画布.移动组件(self, 移动向量.x, 移动向量.y) end
+  def 改变位置(画布, 左点, 右点);画布.改变组件位置(self, 左点.x, 左点.y, 右点.x, 右点.y) end
   定义映射方法(fill: '填充色', offset: '偏移量', stipple: '点画', width: '宽度')
   定义映射方法(activefill: '活动时填充色', disabledfill: '禁用时填充色')
   定义映射方法(activestipple: '活动时点画', disabledstipple: '禁用时点画')
