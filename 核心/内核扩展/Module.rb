bcb321e556f235949a21f3980b9518026f9753c2
shink <shink_7770@qq.com>
2015-10-02 22:57:09 +0800

    中文化

--- 核心/内核扩展/Module.rb
+++ 核心/内核扩展/Module.rb
@@ -3,7 +3,7 @@
 class Module
   alias :_old_method_added_ :method_added
   @@中文化方法映射 = {初始化: 'initialize', 每个: 'each'}
-  @@中文化方法映射.merge!({方法缺失: 'method_missing', 幽灵方法?: 'respond_to_missing?'})
+  @@中文化方法映射.合并!({方法缺失: 'method_missing', 幽灵方法?: 'respond_to_missing?'})
   def method_added(方法名称)
     原方法名 = @@中文化方法映射[方法名称]
     重命名方法(原方法名, 方法名称) if 原方法名
