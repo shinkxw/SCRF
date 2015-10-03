47b9aa688658fb34786ebbbebe824e430bb84d08
shink <shink_7770@qq.com>
2015-10-03 15:14:47 +0800

    中文化

--- 核心/中文化/Thread.rb
+++ 核心/中文化/Thread.rb
@@ -1,8 +1,11 @@
 ﻿#!/usr/bin/env ruby -w
 # encoding: UTF-8
-class << Thread
-  alias :新建 :new
-  alias :当前线程 :current
-  alias :主线程 :main
-  alias :列表 :list
+class Thread
+  class << self
+    alias :新建 :new
+    alias :当前线程 :current
+    alias :主线程 :main
+    alias :列表 :list
+  end
+  alias :字符串 :to_s
 end
