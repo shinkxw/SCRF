63765fec47e0637c98d11dc1cc77ea0a4e27b842
shink <shink_7770@qq.com>
2015-10-15 23:01:43 +0800

    增加中文判断

--- 核心/内核扩展/String.rb
+++ 核心/内核扩展/String.rb
@@ -5,4 +5,5 @@ class String
   def 编码变动(原编码,现编码);self.识别为(原编码).编码为(现编码) end
   def 中文长度;长度 + 编码为('utf-8').扫描(/[\u4E00-\u9FA5]/).大小 end
   def 填充(长度, 填充符 = ' ');中文长度 < 长度 ? self + (填充符 * (长度 - 中文长度)) : self end
+  def 中文?;self =~ /^[\u4e00-\u9fa5]+$/ end
 end
