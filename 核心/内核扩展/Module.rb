814cf159c488018ca7fa52420e202a550495b280
shink <shink_7770@qq.com>
2015-10-15 10:38:10 +0800

    对钩子方法的中文化, 以后需增加日志记录

--- 核心/内核扩展/Module.rb
+++ 核心/内核扩展/Module.rb
@@ -9,10 +9,11 @@ class Module
   alias :_old_method_added_ :method_added
   @@方法映射 = {初始化: 'initialize', 每个: 'each'}
   @@方法映射.合并!({方法缺失: 'method_missing', 幽灵方法?: 'respond_to_missing?'})
+  @@忽略方法名称 = %w(initialize)
   def method_added(方法名称)
     原方法名 = @@方法映射[方法名称]
     if 原方法名
-      if (方法调用限制类型(原方法名) && 方法名称 != :初始化)
+      if (方法调用限制类型(原方法名) && !@@忽略方法名称.包含?(原方法名))
         输出 "#{self}##{原方法名}方法会被覆盖,请注意!"
       end
       重命名方法(原方法名, 方法名称)
@@ -22,12 +23,13 @@ class Module
   end
   alias :_old_singleton_method_added_ :singleton_method_added
   @@单例方法映射 = {常量缺失: 'const_missing'}
+  @@忽略单例方法名称 = %w(const_missing)
   def singleton_method_added(方法名称)
     原方法名 = @@单例方法映射[方法名称]
     当前模块 = self
     单例类.执行 do
       if 原方法名
-        if 方法调用限制类型(原方法名)
+        if (方法调用限制类型(原方法名) && !@@忽略单例方法名称.包含?(原方法名))
           输出 "#{当前模块}.#{原方法名}方法会被覆盖,请注意!"
         end
         重命名方法(原方法名, 方法名称)
