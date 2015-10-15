9efd217f29bbb171fa79fe28cac4a7f7b6ce13f5
shink <shink_7770@qq.com>
2015-10-15 22:42:18 +0800

    待中文化

---
+++ 附加库/官方库/R日志记录器.rb
@@ -0,0 +1,4 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+需要 'logger'
+R日志记录器 = Logger
---
+++ 附加库/官方库/R监视器.rb
@@ -0,0 +1,6 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+#用来保证一个对象在多线程中同时只被一个线程调用
+需要 'monitor'
+R监视器混入 = MonitorMixin
+R监视器 = Monitor
