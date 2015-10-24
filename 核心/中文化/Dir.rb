a7fec1822b2b0dc5a8a9a02fa41654379a7877a0
shink <shink_7770@qq.com>
2015-10-24 17:10:35 +0800

    中文化

---
+++ 核心/中文化/Dir.rb
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class Dir
+  class << self
+    alias :遍历 :foreach
+    alias :打开 :open
+    alias :匹配 :glob
+    alias :存在? :exist?
+    alias :获取当前工作目录? :pwd
+    alias :改变当前工作目录? :chdir
+  end
+  alias :路径 :path
+  alias :每个元素 :each
+  alias :读取 :read
+  alias :关闭 :close
+end
---
+++ 核心/中文化/File.rb
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class File
+  class << self
+    alias :打开 :open#"#{@file_path}","r:#{@encode}"
+    alias :扩展名 :extname
+    alias :存在? :exist?
+    alias :目录? :directory?
+    alias :文件? :file?
+  end
+  alias :路径 :path
+  alias :打开 :open
+  alias :读取 :read
+  alias :逐行读取 :readlines
+  alias :写入 :puts
+  alias :关闭 :close
+end
