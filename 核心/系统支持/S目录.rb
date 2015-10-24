258a25bc17ad2749111e94a908e5d12a3e17f4ed
shink <shink_7770@qq.com>
2015-10-24 17:25:11 +0800

    功能划分

--- 核心/系统支持/S目录.rb
+++ 核心/系统支持/S目录.rb
@@ -1,6 +1,5 @@
 ﻿#!/usr/bin/env ruby -w
 # encoding: UTF-8
-require "fileutils"
 module S目录
   def self.遍历文件路径(目录路径, 编码 = 'UTF-8', 是否递归 = true, &处理)
     子目录路径数组 = []
@@ -22,12 +21,5 @@ module S目录
       处理[子目录路径]
       遍历目录路径(子目录路径, 编码, &处理)
     end
-  end
-  def self.拷贝文件(文件路径, 目标路径);FileUtils.cp(文件路径, 目标路径) end
-  def self.新建文件夹(文件夹路径);FileUtils.mkdir_p(文件夹路径) end
-  def self.删除文件夹(文件夹路径);FileUtils.rmdir(文件夹路径) end
-  def self.清空指定文件夹(文件夹路径)
-    删除文件夹(文件夹路径)
-    新建文件夹(文件夹路径)
   end
 end
---
+++ 附加库/官方库/R文件辅助.rb
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+需要 'fileutils'
+R文件辅助 = FileUtils
+class << R文件辅助
+  alias :拷贝文件 :cp
+  alias :新建文件夹 :mkdir_p
+  alias :删除文件夹 :rmdir
+end
