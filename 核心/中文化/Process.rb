982d5accb75523bd2ec0a4d8c230d0a8c01699f4
shink <shink_7770@qq.com>
2015-12-06 17:08:20 +0800

    增加实时与单调时间

--- 核心/中文化/Process.rb
+++ 核心/中文化/Process.rb
@@ -3,5 +3,6 @@
 module Process
   class << self
     alias :时间 :times
+    alias :获取时钟时间 :clock_gettime
   end
 end
---
+++ 核心/内核扩展/Process.rb
@@ -0,0 +1,8 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+module Process
+  class << self
+    def 实时时钟时间;获取时钟时间(Process::CLOCK_REALTIME) end
+    def 单调时钟时间;获取时钟时间(Process::CLOCK_MONOTONIC) end
+  end
+end
