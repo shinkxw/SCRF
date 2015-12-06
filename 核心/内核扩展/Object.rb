ed3eb581c91b48f9908920edc06cbd60ca35ee79
shink <shink_7770@qq.com>
2015-12-06 17:33:52 +0800

    格式化方式变更, 并去除了Object的格式转换方法

--- 核心/内核扩展/Object.rb
+++ 核心/内核扩展/Object.rb
@@ -2,7 +2,6 @@
 # encoding: UTF-8
 class Object
   def 字符串;puts 类名 << '该类没有覆盖字符串方法';to_s end
-  def 格式转换(格式);R内核.格式化(格式, self) end
   def 类名;self.类.名称 end
   def 定义单例方法(方法名, &块);单例类.强制调用(:定义方法, 方法名, &块) end
   def 定义实例方法(方法名, &块);类.强制调用(:定义方法, 方法名, &块) end
