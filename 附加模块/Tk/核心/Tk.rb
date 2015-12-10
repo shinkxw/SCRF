2e7373d4a46d1489428905a201131845febdabfd
shink <shink_7770@qq.com>
2015-12-10 12:37:33 +0800

    Tk图表功能分离

--- 附加模块/Tk/核心/Tk.rb
+++ 附加模块/Tk/核心/Tk.rb
@@ -3,9 +3,9 @@
 module Tk
   class << self
     alias :运行 :mainloop
-  end
-  def self.后台运行;R线程.新建 {运行} end
-  def self.延迟显示(最大延迟毫秒数 = 300,&运行块)
-    TkAfter.start(最大延迟毫秒数, 1){忽略错误{运行块.执行}}
+    def 后台运行;R线程.新建 {运行} end
+    def 延迟显示(最大延迟毫秒数 = 300,&运行块)
+      TkAfter.start(最大延迟毫秒数, 1){忽略错误{运行块.执行}}
+    end
   end
 end
