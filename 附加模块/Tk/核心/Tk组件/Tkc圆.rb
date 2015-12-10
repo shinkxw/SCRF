129d3127e69f9bb60e850f588c78ff8087e17dfa
shink <shink_7770@qq.com>
2015-12-10 12:03:06 +0800

    增加Tkc组件圆

---
+++ 附加模块/Tk/核心/Tk组件/Tkc圆.rb
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class Tkc圆 < Tkc椭圆
+  def self.创建(画布, 中点, 半径)
+    新建(画布, 中点.x - 半径, 中点.y - 半径, 中点.x + 半径, 中点.y + 半径)
+  end
+  def 改变位置(画布, 中点, 半径)
+    画布.改变组件位置(self, 中点.x - 半径, 中点.y - 半径, 中点.x + 半径, 中点.y + 半径)
+  end
+end
