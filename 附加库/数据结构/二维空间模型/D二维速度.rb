741f8473675b47a378b53ae8a4e07e04b1e1e9df
shink <shink_7770@qq.com>
2016-01-10 20:51:55 +0800

    增加模型结构

---
+++ 附加库/数据结构/二维空间模型/D二维速度.rb
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+需要类 'D二维向量'
+class D二维速度 < D二维向量
+  def 为零?;@x == 0 && @y == 0 end
+  def 不为零?;@x != 0 || @y != 0 end
+  def 置零;@x = 0;@y = 0 end
+  def 移动(时间);self * 时间 end
+  def 减速(数值)
+    速率 = (@x.平方 + @y.平方).开根
+    减速后速率 = 速率 - 数值
+    if 减速后速率 < 0
+      置零
+    else
+      @x = (@x / 速率 * 减速后速率).四舍五入
+      @y = (@y / 速率 * 减速后速率).四舍五入
+    end
+  end
+end
