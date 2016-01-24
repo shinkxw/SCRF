618e6ba6719663a2ad538654ea776fc434565bc1
shink <shink_7770@qq.com>
2016-01-24 21:22:50 +0800

    增加混入强化的功能

---
+++ 核心/扩展/S混入强化.rb
@@ -0,0 +1,38 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+module S混入强化
+  class E多个包含区块 < R标准错误
+    def 初始化;super "不能定义多个包含区块" end
+  end
+
+  def self.被扩展时(基类)
+    基类.设置实例变量(:@_依赖数组, [])
+  end
+
+  def append_features(基类)
+    if 基类.定义了实例变量?(:@_依赖数组)
+      基类.获取实例变量(:@_依赖数组) << self
+      return false
+    else
+      return false if 基类 < self
+      @_依赖数组.每个 {|依赖| 基类.包含模块(依赖)}
+      super
+      基类.扩展 获取常量(:M类方法模块) if 定义了常量?(:M类方法模块)
+      基类.执行(&@_包含时执行代码) if 定义了实例变量?(:@_包含时执行代码)
+    end
+  end
+
+  def included(基类 = nil, &包含时执行代码)
+    if 基类.为无?
+      引发 E多个包含区块 if 定义了实例变量?(:@_包含时执行代码)
+      @_包含时执行代码 = 包含时执行代码
+    else
+      super
+    end
+  end
+
+  def 定义类方法(&方法定义)
+    类方法模块 = 定义了常量?(:M类方法模块, false) ? 获取常量(:M类方法模块) : 设置常量(:M类方法模块, R模块.新建)
+    类方法模块.执行(&方法定义)
+  end
+end
