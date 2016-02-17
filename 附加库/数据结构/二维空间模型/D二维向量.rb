c14d8b32ee89572076d9b42f8f0ede2e7fdf454d
kanxinwei <shink_7770@qq.com>
2016-02-17 17:01:34 +0800

    增加了矩形与圆形间的相交及包围判断

--- 附加库/数据结构/二维空间模型/D二维向量.rb
+++ 附加库/数据结构/二维空间模型/D二维向量.rb
@@ -9,15 +9,18 @@ class D二维向量
   def x反转;类.新建(-@x, @y) end
   def y反转;类.新建(@x, -@y) end
   def 整数化;类.新建(@x.整数化, @y.整数化) end
+  def 正向量;类.新建(@x.绝对值, @y.绝对值) end
   def 零向量?;@x == 0 && @y == 0 end
   def *(数值);类.新建(@x * 数值, @y * 数值) end
   def /(数值);类.新建(@x / 数值, @y / 数值) end
   def +(向量);类.新建(@x + 向量.x, @y + 向量.y) end
   def -(向量);类.新建(@x - 向量.x, @y - 向量.y) end
+  def 取大(数);类.新建(@x.较大(数), @y.较大(数)) end
   def 四舍五入;@x = @x.round;@y = @y.round;self end
   def 字符串;"(#{@x}, #{@y})" end
   def 单位向量;类.新建(@x / 模, @y / 模) end
-  def 模;@模||=(@x.平方 + @y.平方).开根 end
+  def 模;@模 ||= 模平方.开根 end
+  def 模平方;@x.平方 + @y.平方 end
 end
 描述 D二维向量 do
   设置(:随机向量) {D二维向量.随机(100,100)}
