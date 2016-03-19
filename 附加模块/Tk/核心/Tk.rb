1a872184e3f26b4e0eae4c7e37a42d2118420b7c
shink <shink_7770@qq.com>
2016-03-20 00:20:07 +0800

    添加延迟防止出错导致崩溃

--- 附加模块/Tk/核心/Tk.rb
+++ 附加模块/Tk/核心/Tk.rb
@@ -3,7 +3,10 @@
 module Tk
   class << self
     alias :运行 :mainloop
-    def 后台运行;R线程.新建 {运行} end
+    def 后台运行
+      R线程.新建{运行}
+      暂停 0.01
+    end
     def 延迟显示(最大延迟毫秒数 = 300,&运行块)
       TkAfter.start(最大延迟毫秒数, 1){忽略错误{运行块.执行}}
     end
