f723ddd83fc87bc50137c4794412732b7d2ad385
kanxinwei <shink_7770@qq.com>
2016-04-15 11:16:51 +0800

    Use win32-api gem to replace Win32API.

--- 附加库/系统接口扩展/S鼠标.rb
+++ 附加库/系统接口扩展/S鼠标.rb
@@ -2,17 +2,15 @@
 # encoding: UTF-8
 需要 'Win32接口'
 module S鼠标
-  @获取鼠标位置 = Win32接口.新建("user32", "GetCursorPos", ['P'], 'V')
-  @设置鼠标位置 = Win32接口.新建("user32", "SetCursorPos", ['i']*2, 'V')
-  @鼠标事件 = Win32接口.新建("user32", "mouse_event", ['L']*5, 'V')
+  @获取鼠标位置 = Win32接口.新建("GetCursorPos", ['P'], 'V', "user32")
+  @设置鼠标位置 = Win32接口.新建("SetCursorPos", ['I']*2, 'V', "user32")
+  @鼠标事件 = Win32接口.新建("mouse_event", ['L']*5, 'V', "user32")
   def self.位置
     结果 = " " * 8
     @获取鼠标位置.请求(结果)
     结果.解包("LL")
   end
-  def self.设置位置(x, y)
-    @设置鼠标位置.请求(x, y)
-  end
+  def self.设置位置(x, y);@设置鼠标位置.请求(x, y) end
   def self.单击;按下左键;松开左键 end
   def self.双击;单击;单击 end
   def self.右键;按下右键;松开右键 end
--- 附加库/系统接口扩展/Win32接口.rb
+++ 附加库/系统接口扩展/Win32接口.rb
@@ -1,7 +1,7 @@
 ﻿#!/usr/bin/env ruby -w
 # encoding: UTF-8
-需要 'Win32API'
-Win32接口 = Win32API
+需要 'win32/api'
+Win32接口 = Win32::API
 class Win32接口
-  alias :请求 :Call
+  alias :请求 :call
 end
