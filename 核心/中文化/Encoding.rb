9a8d2bc767666afe944c99883f3d82eb8ac1c4d3
kanxinwei <shink_7770@qq.com>
2016-04-21 17:19:49 +0800

    中文化

--- 核心/中文化/Encoding.rb
+++ 核心/中文化/Encoding.rb
@@ -1,5 +1,11 @@
 ﻿#!/usr/bin/env ruby -w
 # encoding: UTF-8
 class Encoding
+  class << self
+    alias :默认内部编码 :default_internal 
+    alias :默认内部编码= :default_internal=
+    alias :默认外部编码 :default_external 
+    alias :默认外部编码= :default_external=
+  end
   alias :字符串 :to_s
 end
