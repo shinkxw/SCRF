456e97049b0392c2d228ec59eae6fdae9ac5c8a5
kanxinwei <shink_7770@qq.com>
2016-04-21 14:43:19 +0800

    中文化

--- 核心/中文化/全局变量中文化.rb
+++ 核心/中文化/全局变量中文化.rb
@@ -2,6 +2,3 @@
 # encoding: UTF-8
 $加载路径 = $LOAD_PATH
 $已加载文件 = $LOADED_FEATURES
-$标准输入 = $stdin
-$标准输出 = $stdout
-$标准错误输出 = $stderr
---
+++ 核心/内核扩展/R标准输入输出.rb
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+module R标准输入输出
+  def self.输入=(输入);$stdin = 输入 end
+  def self.输出=(输出);$stdout = 输出 end
+  def self.错误输出=(输出);$stderr = 输出 end
+end
