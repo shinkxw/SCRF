ca6eb26ff37778344f4badce6a175a74491ddd6d
kanxinwei <shink_7770@qq.com>
2016-05-12 11:31:45 +0800

    增加S操作系统

---
+++ 附加库/系统扩展/S操作系统.rb
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+需要 'Win32接口'
+module S操作系统
+  @获取当前用户 = Win32接口.新建('GetUserName', 'PP', 'I', 'advapi32')
+  模块函数
+  def 当前用户;使用缓冲区{|缓冲区| @获取当前用户.请求(缓冲区, [200].打包('L'))} end
+  def 使用缓冲区;("\0" * 200).自己{|缓冲区| yield 缓冲区}.编码变动('GBK', 'UTF-8').去后空 end
+end
