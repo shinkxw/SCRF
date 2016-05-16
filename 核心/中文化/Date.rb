6ce134a1f57533ec1abbaa480ffdbad2278a01cc
kanxinwei <shink_7770@qq.com>
2016-05-16 16:47:07 +0800

    中文化

---
+++ 核心/中文化/Date.rb
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+require 'date'
+class Date
+  class << self
+    alias :今天 :today
+    alias :解析 :parse
+  end
+  alias :字符串转换 :strftime
+  alias :字符串 :to_s
+end
