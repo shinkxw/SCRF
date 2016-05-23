b28035290f2aaf80ca954ef8ed2a801e32a236eb
kanxinwei <shink_7770@qq.com>
2016-05-23 09:39:43 +0800

    中文化

--- 核心/中文化/Method.rb
+++ 核心/中文化/Method.rb
@@ -2,6 +2,14 @@
 # encoding: UTF-8
 class Method
   alias :调用 :call
+  alias :名称 :name
+  alias :原名 :original_name
+  alias :参数 :parameters
   alias :参数个数 :arity
+  alias :所有者 :owner
+  alias :接收者 :receiver
+  alias :源代码地址 :source_location
+  alias :解除绑定 :unbind
+  alias :函数化 :to_proc
   alias :字符串 :to_s
 end
--- 核心/中文化/Proc.rb
+++ 核心/中文化/Proc.rb
@@ -2,6 +2,9 @@
 # encoding: UTF-8
 class Proc
   alias :调用 :call
+  alias :参数 :parameters
   alias :参数个数 :arity
+  alias :源代码地址 :source_location
+  alias :函数化 :to_proc
   alias :字符串 :to_s
 end
