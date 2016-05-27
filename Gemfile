0843091dbec2e36de8dfa34e9b47235cbf9fdfc7
kanxinwei <shink_7770@qq.com>
2016-05-27 16:02:21 +0800

    增加C电子表格

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'http://gems.ruby-china.org/'
-gem 'win32-api'
-gem 'sys-proctable'
-gem 'wmi-lite'
+gem 'win32-api'#Win32接口
+gem 'sys-proctable'#进程信息
+gem 'wmi-lite'#wmi接口
+gem 'spreadsheet'#excel解析
---
+++ 附加库/软件扩展/C电子表格.rb
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+需要 'spreadsheet'
+C电子表格 = Spreadsheet
+module C电子表格
+  class << self
+    alias :编码 :client_encoding
+    alias :打开 :open
+  end
+  module Excel
+    class Workbook
+      alias :工作表 :worksheet
+      alias :所有工作表 :worksheets
+    end
+    class Worksheet
+      alias :每行 :each
+    end
+    class Row
+      alias :格式 :format
+    end
+  end
+  class Format;end
+end
