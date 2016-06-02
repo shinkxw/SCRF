deb72cbc7357f0d5665647cb41727bd0b4dba799
kanxinwei <shink_7770@qq.com>
2016-06-02 09:06:34 +0800

    添加ERB模板

---
+++ 附加库/官方库/ERB模板.rb
@@ -0,0 +1,11 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+需要 'erb'
+class ERB
+  alias :执行 :run
+  alias :结果 :result
+  alias :代码 :src
+  alias :定义方法 :def_method
+  alias :定义模块 :def_module
+  alias :定义类 :def_class
+end
