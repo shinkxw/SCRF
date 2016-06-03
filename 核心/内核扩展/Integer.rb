90ecccecd3c76db0755f504c17e543209681e211
shink <shink_7770@qq.com>
2016-06-03 21:57:06 +0800

    整数增加均匀划分方法

---
+++ 核心/内核扩展/Integer.rb
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class Integer
+  def 均匀划分(划分数量)
+    商 = self / 划分数量
+    商加一 = 商 + 1
+    余数 = self % 划分数量
+    R数组.新建(划分数量){|索引| 索引 >= 余数 ? 商 : 商加一}
+  end
+end
