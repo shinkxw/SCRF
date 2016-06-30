ef68779c4a1a9c4161518940bc7b69212ab77d68
kanxinwei <shink_7770@qq.com>
2016-06-30 16:25:41 +0800

    增加IO

---
+++ 核心/中文化/IO.rb
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class IO
+  class << self
+    alias :打开 :open
+  end
+  alias :打开 :open
+  alias :读取 :read
+  alias :逐行读取 :readlines
+  alias :写入 :puts
+  alias :关闭 :close
+end
