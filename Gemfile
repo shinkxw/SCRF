5ddb32368dc5598c652b63135d6791968499a480
kanxinwei <shink_7770@qq.com>
2016-07-01 15:31:32 +0800

    增加C性能分析

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ gem 'win32-api'#Win32接口
 gem 'sys-proctable'#进程信息
 gem 'wmi-lite'#wmi接口
 gem 'spreadsheet'#excel解析
+gem 'ruby-prof'#性能分析
---
+++ 附加库/软件扩展/C性能分析.rb
@@ -0,0 +1,29 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+需要 'ruby-prof'
+C性能分析 = RubyProf
+module C性能分析
+  class << self
+    alias :开始 :start
+    alias :暂停 :pause
+    alias :继续 :resume
+    alias :停止 :stop
+    alias :分析 :profile
+  end
+  C文本生成器 = FlatPrinter
+  C详细文本生成器 = FlatPrinterWithLineNumbers
+  C调用图生成器 = GraphPrinter
+  C网页版调用图生成器 = GraphHtmlPrinter
+  Dot格式生成器 = DotPrinter
+  C调用树生成器 = CallTreePrinter
+  C调用栈生成器 = CallStackPrinter
+  C多种生成器 = MultiPrinter
+  class AbstractPrinter
+    def 输出(*参数);print(*参数) end
+    alias :写入 :输出
+  end
+  class MethodInfo
+    alias :old_source_file :source_file
+    def source_file;old_source_file.识别为('UTF-8') end
+  end
+end
