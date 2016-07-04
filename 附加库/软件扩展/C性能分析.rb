3ae19c6fe2c7bebdfc60ca18572895cb5c613a93
kanxinwei <shink_7770@qq.com>
2016-07-04 14:31:40 +0800

    C性能分析优化调用, 增加用例

--- 附加库/软件扩展/C性能分析.rb
+++ 附加库/软件扩展/C性能分析.rb
@@ -2,13 +2,19 @@
 # encoding: UTF-8
 需要 'ruby-prof'
 C性能分析 = RubyProf
+#~ R文件.打开('D:\kanxinwei\Desktop\out.html', 'w'){|文件| C性能分析.分析{ ... }.生成(:调用栈).写入(文件)}
 module C性能分析
   class << self
     alias :开始 :start
     alias :暂停 :pause
     alias :继续 :resume
     alias :停止 :stop
+    alias :结束 :stop
     alias :分析 :profile
+  end
+  C分析结果 = Profile
+  class C分析结果
+    def 生成(生成器类型, *其他参数);C性能分析.获取常量("C#{生成器类型}生成器").新建(self, *其他参数) end
   end
   C文本生成器 = FlatPrinter
   C详细文本生成器 = FlatPrinterWithLineNumbers
