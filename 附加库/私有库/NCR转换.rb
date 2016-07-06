b7b98dbc772196af26f523bae19475370dbb7ade
kanxinwei <shink_7770@qq.com>
2016-07-06 17:19:13 +0800

    增加NCR转换

---
+++ 附加库/私有库/NCR转换.rb
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env ruby -w
+# encoding: UTF-8
+class String
+  def ncr转换;替换(/&#(.*?);/){|值| 值[2..-2].整数化.字符化(R编码::UTF_8)} end
+  def ncr化;每个字符.收集{|字符| "&##{字符.数值化};"}.连接 end
+end
+class NilClass
+  def ncr化;'' end
+end
